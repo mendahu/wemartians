4eec82d89c7941f6645d303d5a201fc8414ee298
Jake Robins <mendahu@gmail.com>
2020-12-27 12:14:10 -0800

    Flesh out template of EpisodeCarousel

--- src/components/EpisodeCarousel/EpisodeCarousel.tsx
+++ src/components/EpisodeCarousel/EpisodeCarousel.tsx
@@ -1,4 +1,5 @@
 import styles from './styles/EpisodeCarousel.module.css';
+import { episodes as episodesCopy } from '../../../copy/copy.json';
 
 export type Episode = {
   number: number;
@@ -14,15 +15,21 @@ export default function EpisodeCarousel({
   episodes = [],
 }: EpisodeCarouselProps) {
   return (
-    <div className={styles.episodeCardContainer}>
-      {episodes.map((episode, index) => {
-        return (
-          <div key={index} className={styles.episodeCard}>
-            <h2>{episode.title}</h2>
-            <p>{episode.description}</p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <h1 className={styles.title}>{episodesCopy.title}</h1>
+      <div className={styles.episodeCardContainer}>
+        {episodes.map((episode, index) => {
+          return (
+            <div key={index} className={styles.episodeCard}>
+              <h2>{episode.title}</h2>
+              <p>{episode.description}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}>{episodesCopy.more}</button>
+      </div>
+    </>
   );
 }
