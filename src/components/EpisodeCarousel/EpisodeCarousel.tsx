17b2e0aea90dae5bbba8d7308f48aa62922b4c78
Jake Robins <mendahu@gmail.com>
2020-12-31 15:57:17 -0800

    Complete Episode Carousel responsive design pattern

--- src/components/EpisodeCarousel/EpisodeCarousel.tsx
+++ src/components/EpisodeCarousel/EpisodeCarousel.tsx
@@ -3,6 +3,8 @@ import { episodes as episodesCopy } from '../../../copy/copy.json';
 import Button from '../Button/Button';
 import Image from 'next/image';
 import { formatDistanceToNow, parseISO } from 'date-fns';
+import classes from './styles/EpisodeCarousel.module.css';
+import classNames from 'classnames';
 
 export type Episode = {
   slug: string;
@@ -22,20 +24,30 @@ export default function EpisodeCarousel({
   return (
     <>
       <h1 className={styles.title}>{episodesCopy.title}</h1>
-      <div className={styles.episodeCardContainer}>
+      <div className={styles.carouselContainer}>
         {episodes.map((episode, index) => {
           const date = parseISO(episode.publishDate);
-          console.log(date);
+
           return (
             <div key={index} className={styles.episodeCard}>
-              <div className={styles.albumArtContainer}>
+              <div
+                className={classNames(
+                  styles.cardContent,
+                  styles.albumArtContainer
+                )}
+              >
                 <Image
                   src={episode.image || '/album_Art_2021-01_400.png'}
-                  width={250}
-                  height={250}
+                  width={510}
+                  height={510}
                 />
               </div>
-              <div className={styles.episodeContent}>
+              <div
+                className={classNames(
+                  styles.cardContent,
+                  styles.episodeContent
+                )}
+              >
                 <h2 className={styles.mtop}>{episode.title}</h2>
                 <p className={styles.mtop}>
                   {episode.description.slice(0, 100) + '...'}
