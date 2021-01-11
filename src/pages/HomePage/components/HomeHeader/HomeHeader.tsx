b7bf1d2bfeeca645264f01cea6a2494c06bba9e9
Jake Robins <me@jakerobins.com>
2021-01-11 12:05:25 -0800

    google, stitcher subscribe buttons

--- src/pages/HomePage/components/HomeHeader/HomeHeader.tsx
+++ src/pages/HomePage/components/HomeHeader/HomeHeader.tsx
@@ -4,6 +4,8 @@ import { header as headerCopy } from "../../../../../copy/Home/index.json";
 import Button from "../../../../components/Button/Button";
 import SubscribeApple from "../../../../components/SubscribeApple/SubscribeApple";
 import SubscribeSpotify from "../../../../components/SubscribeSpotify/SubscribeSpotify";
+import SubscribeGoogle from "../../../../components/SubscribeGoogle/SubscribeGoogle";
+import SubscribeStitcher from "../../../../components/SubscribeStitcher/SubscribeStitcher";
 
 export default function Header(props) {
   return (
@@ -27,14 +29,24 @@ export default function Header(props) {
       <div className={styles.subscribeContainer}>
         <ul className={styles.subscriptionList}>
           <SubscribeApple
-            size={58}
+            size={50}
             url={
               "https://podcasts.apple.com/us/podcast/wemartians-podcast/id1097402685?itsct=podcast_box&itscg=30200"
             }
           />
           <SubscribeSpotify
-            size={58}
+            size={50}
             url={"https://open.spotify.com/show/31p0wkYFE2pdbaXnfhasoL"}
+          />
+          <SubscribeGoogle
+            size={50}
+            url={
+              "https://podcasts.google.com/feed/aHR0cHM6Ly93ZW1hcnRpYW5zLmNvbS9mZWVkL3BvZGNhc3Qv"
+            }
+          />
+          <SubscribeStitcher
+            size={50}
+            url={"https://www.stitcher.com/podcast/wemartians-podcast"}
           />
           {headerCopy.subscriptions.map((sub, index) => (
             <li key={index} className={styles.button}>
