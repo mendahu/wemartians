7a1d9f2f7d8089dfb6611459cdc4926151f6701b
Jake Robins <me@jakerobins.com>
2021-01-11 11:14:18 -0800

    add static redirects

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
 module.exports = {
   images: {
-    domains: ['pbs.twimg.com', 'image.simplecastcdn.com'],
+    domains: ["pbs.twimg.com", "image.simplecastcdn.com"],
   },
-}
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/donors",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/travel-grant",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
