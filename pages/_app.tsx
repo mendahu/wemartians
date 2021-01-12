f892aa27a30aaaa26c82974cd7ffc6c910df18ed
Jake Robins <me@jakerobins.com>
2021-01-12 11:56:53 -0800

    add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,31 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       value={{ episodeId, setEpisodeId, toggleDrawer, handleEpisodeClick }}
     >
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="favicons/safari-pinned-tab.svg"
+          color="#2e2927"
+        />
+        <meta name="msapplication-TileColor" content="#2e2927" />
+        <meta name="theme-color" content="#2e2927" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href={
