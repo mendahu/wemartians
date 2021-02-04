893081a09725578e1ae73ddad78e3da7507a1670
Jake Robins <me@jakerobins.com>
2021-02-04 11:08:33 -0800

    fix signup twitter card

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -4,12 +4,27 @@ import styles from "../../src/pages/SignupPage/styles/SignupPage.module.css";
 import signupCopy from "../../copy/Signup/index.json";
 import Footer from "../../src/components/Footer/Footer";
 import MailingListCallToAction from "../../src/components/MailingListCallToAction/MailingListCallToAction";
+import Head from "next/head";
 
 export type SignupPageProps = {};
 
 export default function index(props: SignupPageProps) {
   return (
     <>
+      <Head>
+        <meta content="text/html; charset=UTF-8" httpEquiv="Content-Type" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@we_martians" />
+        <meta name="twitter:title" content="WeMartians Mailing List" />
+        <meta
+          name="twitter:description"
+          content="Don't miss out on new announcements from Jake."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.wemartians.com/twitter_card_image_wemartians.png"
+        />
+      </Head>
       <Section component="header" background="map">
         <CommonHeader
           title={signupCopy.header.title}
